e9a7c2e80560ab9481de6929ba6017c1db5f8959
jo-315 <32921100+jo-315@users.noreply.github.com>
2018-11-22 23:14:37 +0900

    一時コミット

--- app/src/index.js
+++ app/src/index.js
@@ -1,14 +1,21 @@
 import Vue from "vue"
 import Hello from "./components/App.vue"
 
-document.addEventListener("DOMContentLoaded", () => {
-  new Vue({
-    el: "#app",
+// document.addEventListener("DOMContentLoaded", () => {
+//   new Vue({
+//     el: "#app",
+//
+//     components: {
+//       Hello
+//     },
+//
+//     template: "<Hello />"
+//   })
+// })
 
-    components: {
-      Hello
-    },
-
-    template: "<Hello />"
-  })
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'hellow vue'
+  }
 })
